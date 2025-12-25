a43ce3d342e3a6b71b0fc2f7cdf0755eaec6c229
VivekBardava-OG <vivekbardava@gmail.com>
2025-12-25 23:07:13 +0530

    Mad Lip game added

---
+++ Python/Python Beginner Projects/Mad Lip game.py
@@ -0,0 +1,9 @@
+adjective_1 = str(input("Adjective : "))
+adjective_2 = str(input("Adjective : "))
+noun = str(input("Noun : "))
+verb = str(input("Verb : "))
+past_tense = str(input("Past Tense : "))
+emotion = str (input("Emotion : "))
+
+sent = f"\nToday I went to the {adjective_1} park with my {noun}.\nWe {verb}, {past_tense} near a {adjective_2} tree and felt very {emotion}.\n"
+print(sent)
